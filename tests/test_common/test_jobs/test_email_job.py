65d9325e2ed38b6e77bf94043e2a747ed93e4077
Bakhtiyor Ruziev <bakhtiyor.ruziev@yandex.ru>
2020-01-10 10:17:44 +0300

    Make install devs once

--- tests/test_common/test_jobs/test_email_job.py
+++ tests/test_common/test_jobs/test_email_job.py
@@ -2,25 +2,31 @@ from uuid import uuid4
 
 import pendulum
 import pytest
-from asynctest import Mock
+from async_sender import Mail
+from asynctest import MagicMock, Mock, CoroutineMock
 
-from caffeine.common.schema.mail import EmailMessage
 from caffeine.common.job.email import EmailJob
+from caffeine.common.schema.mail import EmailMessage
 from caffeine.common.store.pubsub import PubSubMessage
 
 
 @pytest.mark.asyncio
 async def test_confirm_email_job():
     subscriber = Mock()
-    email = Mock()
+    email = CoroutineMock(Mail())
+
+    async def send_email(msg):
+        print(msg)
+
+    email.side_effect = send_email
 
     confirm_job = EmailJob(subscriber, email)
-    email_msg = EmailMessage()
+    email_msg = EmailMessage(to="hello@example.com")
     msg = PubSubMessage(
         uuid=str(uuid4()),
         data=email_msg.dict(),
-        created_at=pendulum.now(),
-        updated_at=pendulum.now(),
+        created_at=pendulum.now().int_timestamp,
+        updated_at=pendulum.now().int_timestamp,
         channel="test",
     )
 
